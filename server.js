95788d61f5526f98dddda2451b66b667853373f1
suhas-appirio <suhas.patel@appirio.com>
2017-01-10 17:11:13 +0530

    return response in json format

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express'),
 
     appId = process.env.APP_ID;
 
+var resId = 1;
 console.log(appId);
 
 app.use(bodyParser.json());
@@ -12,22 +13,38 @@ app.use(express.static(__dirname + '/client'));
 
 app.post('/users', function(req, res) {
     var user = req.body.name;
-    res.send( 'hello ' + user + '. you are the coolest person on this earth.' );
+    res.json({
+                "id": resId++,
+                "result": {
+                    "success": true,
+                    "status": 201,
+                    "content": {
+                      "data": 'hello ' + user + '. you are the coolest person on this earth.'
+                    },
+                    "errorContent": null
+                }
+    });
 });
 
 app.all('*', function (req, res, next) {
 
         var targetURL = req.header('Target-URL');
         if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            res.json({
+                "id": resId++,
+                "result": {
+                    "success": false,
+                    "status": 406,
+                    "content": null,
+                    "errorContent": {
+                      "name": "Error",
+                      "message": 'error: There is no Target-Endpoint header in the request',
+                      "forwardUrl": null
+                    }
+                }
+    });
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-            }).pipe(res);
 });
 
 app.set('port', process.env.PORT || 8200);
